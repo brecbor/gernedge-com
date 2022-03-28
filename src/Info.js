ba458883ecbb362386238b650105bff14bb3f89c
bor brecelj <bor.brecelj@gmail.com>
2022-03-29 01:19:06 +0200

    Fix line spacing in info

--- src/Info.js
+++ src/Info.js
@@ -6,24 +6,24 @@ import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
 import { Link as RouterLink } from 'react-router-dom';
 import {ScrollToTop} from './ScrollToTop';
-import {BasePage, NavBar, ArchiveButton} from './elements';
+import { BasePage, NavBar, ArchiveButton, TopAlignedText } from './elements';
 
 function Info() {
   return (
     <BasePage title='Info'>
-      <VStack spacing='96px' m='24px' textStyle='normal' fontSize='32px' textAlign='justify'>
+      <VStack spacing='96px' m='24px' textStyle='normal' fontSize='32px' textAlign='justify' lineHeight='48pt'>
         <VStack spacing='48px'>
-          <Text>Jernej Šimec (1997) — an inquisitive visual artist using light as a tool for exploring the common
+          <TopAlignedText>Jernej Šimec (1997) — an inquisitive visual artist using light as a tool for exploring the common
             ground between photography and humanity, thus reinterpreting the notion of existence and challenging our
             perceptions of nature. During his studies, he completed a full year at the Academy of Fine Arts in Warsaw
             where he also attended a 5-month workshop Art Made by Walking, which influenced his work by blurring the
             line between life and art. Since 2021 he is contributing photos
             to <Link color='#6cffd7' href='https://postplaneta.com/' isExternal>POST_PLANETA</Link>
             . He is available for
-            commissioned work all around the world.</Text>
-          <Text width='100%'>For inquiries on prints, collaborations or commissioned work please contact me
+            commissioned work all around the world.</TopAlignedText>
+          <TopAlignedText width='100%'>For inquiries on prints, collaborations or commissioned work please contact me
             on <Link color='#6cffd7' href='mailto:jernej.simec@gmail.com' isExternal>e-mail</Link> or <Link color='#6cffd7' href='https://www.instagram.com/jernejsimec/' isExternal>Instagram</Link>.
-          </Text>
+          </TopAlignedText>
         </VStack>
         <Box m='24px' width='100%'>
           <Image width='50vw' src='images/tmp.jpg' alt='jernej'/>
