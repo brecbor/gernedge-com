40a18a46aad3bba01c8929c05e5b2cd320443644
bor brecelj <bor.brecelj@gmail.com>
2022-04-05 19:58:27 +0200

    Move project 45, 14 to a separate component

--- src/ProjectPage.js
+++ src/ProjectPage.js
@@ -9,6 +9,7 @@ import { Link as RouterLink } from 'react-router-dom';
 import {ScrollToTop} from './ScrollToTop';
 import {BasePage, NavBar, ArchiveButton} from './elements';
 import Archive from './Archive';
+import Project_45_14 from './Project_45_14';
 
 
 const content = {
@@ -21,68 +22,7 @@ const content = {
   '45_14': {
     'title': '45.588620, 14.447503',
     'content': (
-      <VStack spacing={0}>
-        <Grid templateRows='repeat(5, 15.63vw)' templateColumns='repeat(6, 1fr)' gap='0' >
-          <GridItem rowStart={1} rowEnd={5} colStart={5} colEnd={7} >
-            <Image pl='1.25vw' width='100%' src='/images/45_14/1.jpg' alt='45_14/1'/>
-          </GridItem>
-          <GridItem rowStart={1} rowEnd={6} colStart={3} colEnd={5} >
-            <Image pl='1.25vw' pr='1.25vw' pt='24.06vw' width='100%' src='/images/45_14/2.jpg' alt='45_14/1'/>
-          </GridItem>
-          <GridItem rowStart={1} rowEnd={6} colStart={1} colEnd={2} >
-            <Image pr='1.25vw' pt='47.19vw' width='100%' src='/images/45_14/3.jpg' alt='45_14/1'/>
-          </GridItem>
-        </Grid>
-        {/*<Box width='100%' height='50vh'/>*/}
-        <Image pt='50vh' width='100%' src='/images/45_14/4.jpg' alt='45_14/1'/>
-        <Image pt='50vh' width='100%' src='/images/45_14/6.png' alt='45_14/1'/>
-        {/*<Grid width='100%' templateRows='repeat(5, 15.63vw) 50vh repeat(5, 15.63vw)' templateColumns='repeat(6, 1fr)' gap='0' >
-          <GridItem bg='tomato' />
-          <GridItem bg='papayawhip' />
-          <GridItem bg='papayawhip' />
-          <GridItem bg='tomato' />
-          <GridItem bg='tomato' />
-          <GridItem bg='papayawhip' />
-          <GridItem bg='papayawhip' />
-          <GridItem bg='tomato' />
-          <GridItem bg='tomato' />
-          <GridItem bg='papayawhip' />
-          <GridItem bg='tomato' />
-          <GridItem bg='papayawhip' />
-          <GridItem bg='tomato' />
-          <GridItem bg='papayawhip' />
-          <GridItem bg='papayawhip' />
-          <GridItem bg='tomato' />
-          <GridItem bg='papayawhip' />
-          <GridItem bg='tomato' />
-          <GridItem bg='papayawhip' />
-          <GridItem bg='tomato' />
-          <GridItem bg='tomato' />
-          <GridItem bg='papayawhip' />
-          <GridItem bg='tomato' />
-          <GridItem bg='papayawhip' />
-          <GridItem bg='tomato' />
-          <GridItem bg='papayawhip' />
-          <GridItem bg='papayawhip' />
-          <GridItem bg='tomato' />
-          <GridItem bg='papayawhip' />
-          <GridItem bg='tomato' />
-          <GridItem bg='papayawhip' />
-          <GridItem bg='tomato' />
-          <GridItem bg='tomato' />
-          <GridItem bg='papayawhip' />
-          <GridItem bg='tomato' />
-          <GridItem bg='papayawhip' />
-          <GridItem bg='tomato' />
-          <GridItem bg='papayawhip' />
-          <GridItem bg='papayawhip' />
-          <GridItem bg='tomato' />
-          <GridItem bg='papayawhip' />
-          <GridItem bg='tomato' />
-          <GridItem bg='papayawhip' />
-          <GridItem bg='tomato' />
-        </Grid>*/}
-      </VStack>
+      <Project_45_14/>
     )
   },
 };
