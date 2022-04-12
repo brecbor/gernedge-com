f91a2a6062334a6bedb7e338796d739fa753e62d
bor brecelj <bor.brecelj@gmail.com>
2022-04-12 14:48:51 +0200

    Add field recording project

--- src/ProjectPage.js
+++ src/ProjectPage.js
@@ -25,6 +25,25 @@ const content = {
       <Project_45_14/>
     )
   },
+  'field_recording': {
+    'title': 'FR_200607, 2021',
+    'content': (
+      <Box width='100%' position='relative' p='56.25% 0 0 0'>
+        <iframe src="https://player.vimeo.com/video/655024508?h=e226ff68bd&autoplay=1&title=0&byline=0&portrait=0"
+                style={{width: '100%', height: '100%', position: 'absolute', top: 0, left: 0}/*"position:absolute;top:0;left:0;width:100%;height:100%;"*/} frameBorder="0"
+                allow="autoplay; fullscreen; picture-in-picture" allowFullScreen></iframe>
+        <script src="https://player.vimeo.com/api/player.js"></script>
+      {/*<iframe src="https://player.vimeo.com/video/655024508?h=e226ff68bd&autoplay=1" width="100%"
+              frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen></iframe>*/}
+        {/*<div style="padding:56.25% 0 0 0;position:relative;">
+        <iframe src="https://player.vimeo.com/video/655024508?h=e226ff68bd&autoplay=1&title=0&byline=0&portrait=0"
+        style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0"
+         allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+         </div>
+         <script src="https://player.vimeo.com/api/player.js"></script>*/}
+      </Box>
+    )
+  },
 };
 
 
