b72f9dcd0781cf6db0537060cb85439bbf373456
bor brecelj <bor.brecelj@gmail.com>
2022-04-12 16:53:20 +0200

    Add project memory induced

--- src/ProjectPage.js
+++ src/ProjectPage.js
@@ -37,6 +37,17 @@ const content = {
       </Box>
     )
   },
+  'memory_induced': {
+    'title': 'Memory Induced, colour video and sound, 2020',
+    'content': (
+      <Box mx='auto' mt={'0'/*'31.25vw'*/} width='50%' position='relative' p='37.5% 0 0 0'>
+        <iframe src="https://player.vimeo.com/video/600951447?h=1939fc6cc1&autoplay=1&title=0&byline=0&portrait=0"
+                style={{width: '100%', height: '100%', position: 'absolute', top: 0, left: 0}} frameBorder="0"
+                allow="autoplay; fullscreen; picture-in-picture" allowFullScreen></iframe>
+        <script src="https://player.vimeo.com/api/player.js"></script>
+      </Box>
+    )
+  },
 };
 
 
