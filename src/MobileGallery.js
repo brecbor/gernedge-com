875c34e768ac318dfa49ff07b3fc58d8f47f6899
bor brecelj <bor.brecelj@gmail.com>
2022-05-05 12:04:28 +0200

    Change dots under images to numbers

--- src/MobileGallery.js
+++ src/MobileGallery.js
@@ -26,7 +26,7 @@ function MobileGallery(props) {
     onSwipedRight: (eventData) => setIndex((index + n - 1) % n),
   });
 
-  let empty_dot='○', full_dot='●', dots = [];
+  /*let empty_dot='○', full_dot='●', dots = [];
   for(let i=0; i<n; i++) {
     if(i == index) {
       dots.push(
@@ -42,16 +42,14 @@ function MobileGallery(props) {
         </IconContext.Provider>
       );//<BsCircle w='8px' h='8px'/>);//<Text lineHeight='24px'  fontSize='16px'>{empty_dot}</Text>);
     }
-  }
+  }*/
 
   return (
     <Center height='100%' {...handlers}>
       <VStack px='16px' width='100%' spacing='16px'>
         <Image src={props.images[index]} width='100%' p={props.imageBorder ? '0.5%' : 0} bgColor='white'/>
-        <HStack>
-          {dots/*<Text mt='2px' lineHeight='24px' fontSize='10px'>{full_dot}</Text><Text lineHeight='24px'  fontSize='16px'>{empty_dot}</Text><Text lineHeight='24px'  fontSize='16px'>{empty_dot}</Text><Text fontSize='16px'>{empty_dot}</Text><Text fontSize='16px'>{empty_dot}</Text><Text fontSize='16px'>{empty_dot}</Text>*/}
-        </HStack>
-        </VStack>
+        <Text fontSize='8px'>{(index+1).toString() + '/' + n}</Text>
+      </VStack>
     </Center>
   );
 }
